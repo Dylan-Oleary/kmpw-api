7f2fb9b30f125cf85b209f5e693b8b62a84a71d0
Dylan O'Leary <dylanolearydev@gmail.com>
2021-12-05 12:21:39 -0500

    Refactor Prisma schema to conform to attribute relationships

--- prisma/migrations/20211205171908_init/migration.sql
+++ prisma/migrations/20211205171908_init/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `BreedGroup` (
     `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
     `name` VARCHAR(191) NOT NULL,
     `description` TEXT NULL,
 
@@ -14,6 +15,7 @@ CREATE TABLE `Breed` (
     `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
     `name` VARCHAR(191) NOT NULL,
     `description` TEXT NULL,
     `altNames` VARCHAR(191) NULL,
@@ -45,28 +47,52 @@ CREATE TABLE `Breed` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `BreedAttribute` (
+CREATE TABLE `DogAttribute` (
     `id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
+    `isGeneratedByUser` BOOLEAN NOT NULL DEFAULT false,
     `label` VARCHAR(191) NOT NULL,
     `value` VARCHAR(191) NOT NULL,
     `type` ENUM('BRED_FOR', 'TEMPERAMENT') NOT NULL,
-    `includeTemperamentId` VARCHAR(191) NULL,
-    `includeBredForId` VARCHAR(191) NULL,
-    `breedId` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `DogAttributesOnBreeds` (
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
+    `breedId` VARCHAR(191) NOT NULL,
+    `dogAttributeId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`breedId`, `dogAttributeId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `DogAttributesOnBreedGroups` (
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `isDeleted` BOOLEAN NOT NULL DEFAULT false,
+    `breedGroupId` VARCHAR(191) NOT NULL,
+    `dogAttributeId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`breedGroupId`, `dogAttributeId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `Breed` ADD CONSTRAINT `Breed_breedGroupId_fkey` FOREIGN KEY (`breedGroupId`) REFERENCES `BreedGroup`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `BreedAttribute` ADD CONSTRAINT `BreedAttribute_includeTemperamentId_fkey` FOREIGN KEY (`includeTemperamentId`) REFERENCES `Breed`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `DogAttributesOnBreeds` ADD CONSTRAINT `DogAttributesOnBreeds_breedId_fkey` FOREIGN KEY (`breedId`) REFERENCES `Breed`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `BreedAttribute` ADD CONSTRAINT `BreedAttribute_includeBredForId_fkey` FOREIGN KEY (`includeBredForId`) REFERENCES `Breed`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `DogAttributesOnBreeds` ADD CONSTRAINT `DogAttributesOnBreeds_dogAttributeId_fkey` FOREIGN KEY (`dogAttributeId`) REFERENCES `DogAttribute`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `BreedAttribute` ADD CONSTRAINT `BreedAttribute_breedId_fkey` FOREIGN KEY (`breedId`) REFERENCES `Breed`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `DogAttributesOnBreedGroups` ADD CONSTRAINT `DogAttributesOnBreedGroups_breedGroupId_fkey` FOREIGN KEY (`breedGroupId`) REFERENCES `BreedGroup`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `DogAttributesOnBreedGroups` ADD CONSTRAINT `DogAttributesOnBreedGroups_dogAttributeId_fkey` FOREIGN KEY (`dogAttributeId`) REFERENCES `DogAttribute`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
