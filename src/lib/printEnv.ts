e895e1f8e6ccbc05c436fcbd729dec7563a47e35
Dylan O'Leary <dylanolearydev@gmail.com>
2022-02-25 21:15:43 -0500

    Update environment logging

--- src/lib/printEnv.ts
+++ src/lib/printEnv.ts
@@ -11,6 +11,7 @@ const printEnv: () => void = () => {
     console.info(":".repeat(packageName.length));
     console.info("");
     console.info(`Environment: ${process.env.NODE_ENV}`);
+    console.info("");
 
     const envKeys = [
         "PORT",
@@ -18,7 +19,9 @@ const printEnv: () => void = () => {
         "ACCESS_TOKEN_SECRET",
         "ACCESS_TOKEN_LIFESPAN",
         "REFRESH_TOKEN_SECRET",
-        "REFRESH_TOKEN_LIFESPAN"
+        "REFRESH_TOKEN_LIFESPAN",
+        "DOG_API_BASE_URL",
+        "WEATHER_API_BASE_URL"
     ];
 
     envKeys.sort();
@@ -30,6 +33,8 @@ const printEnv: () => void = () => {
     envKeys.forEach((key) => {
         console.info(`ENV: ${key.padEnd(longestKey.length, " ")} -> ${process.env[key]}`);
     });
+
+    console.info("");
 };
 
 export default printEnv;
